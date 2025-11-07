ec48cf5660201ec104a950d500d3175cdbc2cfe0
eloy garofano <eloygarov@gmail.com>
2025-11-07 10:13:20 +0100

    actualizo la screen de galio

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:fl_components/routes/app_routes.dart';
 import 'package:fl_components/screens/screens.dart';
 import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
-
 import 'routes/app_routes.dart';
 
 void main() => runApp(const MyApp());
--- lib/screens/galio_screen.dart
+++ lib/screens/galio_screen.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 class GalioScreen extends StatelessWidget {
    
   const GalioScreen({Key? key}) : super(key: key);
+
+  final String galioDescription = "Galio es un coloso de piedra que vigila las afueras de Demacia y"+
+         "protege a su ciudad de la magia. Fue construido por el escultor Durand para ser un"+
+         "baluarte contra los magos enemigos, permanece inmóvil hasta que la presencia de magia"+
+         "poderosa lo despierta. Sus habilidades le permiten infligir daño masivo en área,"+
+         "mientras que su escudo le otorga defensa y control de masas a través de una provocación."+
+         "Su definitiva (Entrada heroica) le permite volar hacia un aliado, lo que le otorga reducción de"+
+         "daño y crea una onda de choque que derriba a los enemigos";
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-         child: Text('Galio'),
+      appBar: AppBar(
+        title: Text('Campeones: Galio'),
+        backgroundColor: Color(0xFFE3F5E9) //0xFF antes del color a usar
+      ),
+      body: Column(
+        children: [
+          Text("Galio Info"),
+          //AQUI VA LA IMAGEN
+          Text(galioDescription)
+        ],
       ),
     );
   }
