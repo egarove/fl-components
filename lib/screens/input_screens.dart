969531b52863125dfbed279bc00ffe5cde54a84d
eloy garofano <eloygarov@gmail.com>
2025-11-28 09:50:40 +0100

    solo se envian los datos si estan los campos validados

--- lib/screens/input_screens.dart
+++ lib/screens/input_screens.dart
@@ -7,6 +7,18 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final myFormKey = GlobalKey<FormState>();
+    
+    
+    final Map<String,String> formData = {
+      'nombre': 'Tahm',
+      'apellidos' : 'Kench',
+      'email' : 'tahmkench@aguasestancadas.com',
+      'password' : '12345',
+      'rol' : 'usuario',
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Input Forms"),
@@ -14,6 +26,7 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
         child: Form(
+          key: myFormKey, //key del formulario que definimos en la linea 11
           child: Column(
             children: [
               CustomTextFormField(
@@ -59,7 +72,13 @@ class InputsScreen extends StatelessWidget {
                   child: Center(child: Text("Enviar")),
                   width: double.infinity,
                 ),
-                onPressed: () => {},
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(FocusNode()); //cuando le demos a "enviar" se quita el teclado
+                  if( !myFormKey.currentState!.validate() ){
+                    print('Formulario no valido');
+                  } 
+                  print(formData);
+                },
               )       
             ],          
           ),
