209879304aeb5060d332d2118da5cf155724ce55
eloy garofano <eloygarov@gmail.com>
2025-12-11 10:11:42 +0100

    simulando 3 segundos de espera

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,9 +1,63 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
    
   const ListViewBuilderScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+
+/**
+ * como hacer scroll infinito
+ */
+  final List<int> imageIds = [1,2,3,4,5,6,7,8,9,10];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      print(' ${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+      if(scrollController.position.pixels >= scrollController.position.maxScrollExtent){
+        fetchData();
+      }
+    });
+  }
+
+  Future fetchData() async { //todos los metodos Future son async
+    if (isLoading) return; //si ya estamos cargando no vamos a cargar de nuevo
+
+    isLoading = true;
+    setState(() {
+      
+    });
+
+    await Future.delayed(Duration(seconds: 3)); //simula una espera de 3 segundos pero al ser async no bloquea la app
+    add5();
+
+    isLoading = false;
+
+    setState(() {
+      
+    });
+  }
+
+
+  void add5() {
+    int lastId = imageIds.last;
+    for (int i=0 ; i<5; i++){
+      imageIds.add(lastId+i);
+    }
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,14 +66,15 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
-          itemCount: 10,
+          controller: scrollController,
+          itemCount: imageIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
               width: double.infinity,
               height: 250,
               fit: BoxFit.cover,
               placeholder: AssetImage("assets/jar-loading.gif"),
-              image: NetworkImage("https://picsum.photos/500/600?image=$index")
+              image: NetworkImage("https://picsum.photos/500/600?image=${imageIds[index]}")
             );
           },
         ),
